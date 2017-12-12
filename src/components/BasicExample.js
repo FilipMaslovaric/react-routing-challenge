83bd2e21b3421fe7e921725c0f72167da167cf7e
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-12-12 12:46:43 +1000

    Bug fix: wrapped all routes with a <Switch> and imported Switch from react-router to handle 404 correctly

--- src/components/BasicExample.js
+++ src/components/BasicExample.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    Switch
 } from 'react-router-dom';
 import NoMatch from './NoMatch';
 import Home from './Home';
@@ -21,12 +22,13 @@ const BasicExample = () => (
             </ul>
 
             <hr />
-
+        <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/topics" component={Topics} />
             <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
+        </Switch>
         </div>
     </Router>
 )
